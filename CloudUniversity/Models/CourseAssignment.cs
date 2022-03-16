6faea38a931dd74273648138f123a8703e65b58a
Charles Akunafia <charlytp247@gmail.com>
2022-03-16 01:50:00 +0100

    Add project files.

---
+++ CloudUniversity/Models/CourseAssignment.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudUniversity.Models
+{
+    public enum TestScore
+    {
+        A, B, C, D, F
+    }
+    public enum ExamScore
+    {
+        A, B, C, D, F
+    }
+    public enum CGPA
+    {
+        A, B, C, D, F
+    }
+
+    public class CourseAssignment
+    {
+        [Display(Name = "Enrollment ID")]
+        //[ForeignKey("EnrollmentID")]
+        public int? EnrollmentID { get; set; }
+        public Enrollment Enrollment { get; set; }
+
+        [Display(Name = "Test Score")]
+     
+        public TestScore? TestScore { get; set; }
+        [Display(Name = "Exam Score")]
+       
+        public ExamScore? ExamScore { get; set; }
+        [Display(Name = "Final Grade")]
+       
+        public CGPA? CGPA { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Comments")]
+        public string Location { get; set; }
+        
+       
+    }
+}
---
+++ CloudUniversity/Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudUniversity.Models
+{
+ 
+
+    public class Enrollment
+    {
+        [Display(Name = "Enrollment ID")]
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        [ForeignKey("StudentID")]
+        public int StudentID { get; set; }
+
+        public virtual Student Student { get; set; }
+        //[DisplayFormat(NullDisplayText = "No grade")]
+
+        public Course Course { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+    }
+}
